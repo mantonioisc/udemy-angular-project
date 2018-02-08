fdb2ec59618f119a90a75a7be6c2605e9bcc55a2
mantonioisc <mantonioisc@outlook.com>
2018-02-08 00:44:45 -0600

    Section 10 (104-111) - Using services to communicate Components and simplifying communication removing a lot of events

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,19 +9,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/2007feb-sushi-odaiba-manytypes.jpg/640px-2007feb-sushi-odaiba-manytypes.jpg'),
-    new Recipe('Another Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Terrine_de_saumon_au_basilic.JPG/800px-Terrine_de_saumon_au_basilic.JPG')
-  ];
+  constructor(private recipeService: RecipeService) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,30 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('Sushi', 'Raw fished in single portions', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/2007feb-sushi-odaiba-manytypes.jpg/640px-2007feb-sushi-odaiba-manytypes.jpg', [
+      new Ingredient('Salmon', 1),
+      new Ingredient('Tuna', 1),
+      new Ingredient('Shrimp', 1)
+    ]),
+    new Recipe('Terrine', 'French forcemeat loaf', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Terrine_de_saumon_au_basilic.JPG/800px-Terrine_de_saumon_au_basilic.JPG', [
+      new Ingredient('Salmon', 2)
+    ])
+  ];
+
+  constructor(private shoppingListServive: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice(); //don't leak "recipes" reference
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListServive.addIngredients(ingredients);
+  }
+}
